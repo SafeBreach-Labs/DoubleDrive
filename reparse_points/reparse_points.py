488066f2ce0fd8d3f0ec6ee08e86f531aa546ce0
Or Yair <or.yair@safebreach.com>
2023-06-18 13:37:44 +0300

    added nt_path function

--- reparse_points/reparse_points.py
+++ reparse_points/reparse_points.py
@@ -2,12 +2,19 @@ import win32file
 import winioctlcon
 import ctypes
 from .reparse_structs import *
-from file_utils import nt_path
 import pywintypes
 
 IO_REPARSE_TAG_SYMLINK = 0xA000000C
 IO_REPARSE_TAG_MOUNT_POINT = 0xA0000003
 REPARSE_DATA_BUFFER_HEADER_LENGTH = getattr(REPARSE_DATA_BUFFER, "GenericReparseBuffer").offset
+
+
+def nt_path(path: str):
+    if path.startswith("\\??"):
+        return path
+    
+    return f"\\??\\{path}"
+
 
 def create_symlink_reparse_buffer(target_path: str, print_name: str, relative: bool):
     if not relative: target_path = nt_path(target_path)
@@ -40,6 +47,7 @@ def create_symlink_reparse_buffer(target_path: str, print_name: str, relative: b
     reparse_data_struct.SymbolicLinkReparseBuffer.Flags = 1 if relative else 0
 
     return bytes(reparse_data_buffer)
+
 
 def create_mount_point_reparse_buffer(target_path: str, print_name: str, relative: bool):
     if not relative: target_path = nt_path(target_path)
@@ -87,10 +95,12 @@ def set_reparse_point(reparse_point_path, reparse_data_buffer, is_dir=False):
     reparse_point_handle = win32file.CreateFile(reparse_point_path, win32file.GENERIC_READ | win32file.GENERIC_WRITE, 0, None, win32file.OPEN_EXISTING, file_flags, 0)
     win32file.DeviceIoControl(reparse_point_handle, winioctlcon.FSCTL_SET_REPARSE_POINT, reparse_data_buffer, None, None)
 
+
 def create_ntfs_symlink(reparse_point_path, target_path, relative=False, print_name=None, is_dir=False):
     if None == print_name: print_name = target_path
     reparse_data_buffer = create_symlink_reparse_buffer(target_path, print_name, relative)
     set_reparse_point(reparse_point_path, reparse_data_buffer, is_dir)
+
 
 def create_mount_point(reparse_point_path, target_path, relative=False, print_name=None):
     if None == print_name: print_name = target_path
